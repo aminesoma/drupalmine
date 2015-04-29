8f185414b93cda495ec20065f58d940a3ef38645
amine <koudri.amine@gmail.com>
2015-04-29 11:17:02 +0000

    Cleaning Theme block and views file

--- sites/all/themes/ipress/templates/views-view--ipress-blocks--top-news.tpl.php
+++
@@ -1,7 +0,0 @@
-  <?php print render($title_prefix); ?>
-<?php if ($header): ?>
-      <?php print $header; ?>
-  <?php endif; ?>
-<?php if ($rows): ?>
-      <?php print $rows; ?>
-  <?php endif; ?>
